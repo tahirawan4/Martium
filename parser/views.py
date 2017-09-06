7d12d71e64f16c66bec1deb6e868344cfdf3f343
Ahmed Hassan Khan <kakarstack@gmail.com>
2017-09-07 00:29:40 +0500

    models

--- parser/views.py
+++ parser/views.py
@@ -2,7 +2,48 @@ from django.http import HttpResponse
 import json
 
 
+# def index(request):
+#     # TODO: we will use serializers instead
+#     data = {'page': 'index'}
+#     return HttpResponse(json.dumps(data), content_type="application/json")
+
+
+
+from django.http import HttpResponse
+from django.http import JsonResponse
+import json
+import requests
+from parser.constants import feedly_api_url,feedly_get_categories_url,feedly_authrization_url,dev_token,client_id
+
+
 def index(request):
     # TODO: we will use serializers instead
-    data = {'page': 'index'}
+    feed_response_data = requests.get(feedly_api_url)
+
+    data = feed_response_data.text
+
     return HttpResponse(json.dumps(data), content_type="application/json")
+
+
+def dash_board(request):
+    # TODO: This view is call from the url and will return the Feedly fetch records to html view
+    # url(r'^dashboard$', dash_board, name='dash_board'),
+
+    feed_response_data = requests.get(feedly_api_url)
+
+    data = feed_response_data.text
+    return JsonResponse(data, safe=False)
+
+
+def get_categories(request):
+    # TODO: This view is call from the url and will return the Feedly fetch records to html view
+    # url(r'^dashboard$', dash_board, name='dash_board'),
+
+    feed_response_data = requests.post(feedly_get_categories_url, headers = {'authorization': dev_token})
+
+
+    # feed_response_data = requests.get(feedly_get_categories_url)
+
+
+    data = feed_response_data.text
+    return JsonResponse(data, safe=False)
