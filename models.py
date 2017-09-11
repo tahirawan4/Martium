1b69b8e2f9c17cd9972f44a678b5592a8c4b54d3
Ahmed Hassan Khan <kakarstack@gmail.com>
2017-09-11 23:14:53 +0500

    chnages to sandbox7

--- models.py
+++ models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 
 
+
 class TimeStampedModel(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
