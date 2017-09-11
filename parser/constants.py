e248a4c40ddf43fbd53d429724c393918a128b9a
aitzazabid <aitzaz.abid@gmail.com>
2017-09-11 23:17:42 +0500

    Update constants.py
    changes the url
    replace sandbox to sandbox7

--- parser/constants.py
+++ parser/constants.py
@@ -2,9 +2,9 @@
 
 
 
-FEEDLY_API_URL = 'http://sandbox.feedly.com/v3/streams/contents?streamId=feed%2Fhttp%3A%2F%2Ffeeds.feedburner.com%2Ftrdnews'
+FEEDLY_API_URL = 'http://sandbox7.feedly.com/v3/streams/contents?streamId=feed%2Fhttp%3A%2F%2Ffeeds.feedburner.com%2Ftrdnews'
 
-FEEDLY_GET_CATEGORIES_URL = 'http://sandbox.feedly.com/v3/categories/Finance'
+FEEDLY_GET_CATEGORIES_URL = 'http://sandbox7.feedly.com/v3/categories/Finance'
 
 DEV_TOKEN ='AzZVWr0jpfS3DDqq1NgCHgYRnmpzuHDsYPC-3Y6wZj6KBIhaD3f9njOuQ6cqSLMnl_p4FBS96tqggIn5JXgvzszQzKlnOsPkpKgDqYezfYWRa2yd9Itm_sM0Vwkp-bQ17bqZ6r0bMOQGwpPnV7k6WY5ebCl2_wZwgcxADiyBwr_KWQJ7equhbsOlU3dSClej-lK-5AJ5fddURsG7zBnYr2bt93lftw:feedlydev'
 
