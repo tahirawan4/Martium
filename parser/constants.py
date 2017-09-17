e923344673df6cfef3c1d68841e0b3b23b796139
tahirawan49 <tahir.fazal@arbisoft.com>
2017-09-18 01:48:43 +0500

    adding models and migrations

--- parser/constants.py
+++ parser/constants.py
@@ -1,4 +1,6 @@
-FEEDLY_API_URL = 'http://sandbox7.feedly.com/v3/streams/contents?streamId=feed%2Fhttp%3A%2F%2Ffeeds.feedburner.com%2Ftrdnews'
+
+FEEDLY_API_URL = 'http://cloud.feedly.com/v3/streams/contents?streamId=feed%2Fhttp%3A%2F%2Ffeeds.feedburner.com%2Ftrdnews'
+#'http://sandbox7.feedly.com/v3/streams/contents?streamId=feed%2Fhttp%3A%2F%2Ffeeds.feedburner.com%2Ftrdnews'
 FEEDLY_GET_CATEGORIES_URL = 'http://sandbox7.feedly.com/v3/categories/Finance'
 DEV_TOKEN = 'AzZVWr0jpfS3DDqq1NgCHgYRnmpzuHDsYPC-3Y6wZj6KBIhaD3f9njOuQ6cqSLMnl_p4FBS96tqggIn5JXgvzszQzKlnOsPkpKgDqYezfYWRa2yd9Itm_sM0Vwkp-bQ17bqZ6r0bMOQGwpPnV7k6WY5ebCl2_wZwgcxADiyBwr_KWQJ7equhbsOlU3dSClej-lK-5AJ5fddURsG7zBnYr2bt93lftw:feedlydev'
 CLIENT_ID = '3306a8e8-2885-4750-829b-30d15549ecdb.'
--- parser/models.py
+++ parser/models.py
@@ -1,120 +1,45 @@
-# from django.db import models
-# from django.contrib.auth.models import User
-# from django.core.urlresolvers import reverse
-# 
-# 
-# 
-# class TimeStampedModel(models.Model):
-#     created = models.DateTimeField(auto_now_add=True)
-#     modified = models.DateTimeField(auto_now=True)
-#     isRemove = models.BooleanField(default=False)
-#     class Meta:
-#         abstract = True
-# 
-# 
-# class Author(models.Model):
-#     user = models.ForeignKey(User, related_name='author',db_index=True)
-#     avatar = models.ImageField(upload_to='gallery/avatar/%Y/%m/%d',
-#                                null=True,
-#                                blank=True,
-#                                help_text="Upload your photo for Avatar")
-#     about = models.TextField()
-#     website = models.URLField(max_length=200, blank=True, null=True)
-#     is_remove = models.BooleanField(default=False)
-# 
-#     def __str__(self):
-#         return self.user.username
-# 
-#     def get_absolute_url(self):
-#         return reverse('author_posts_page',
-#                        kwargs={'username': self.user.username})
-# 
-#     class Meta:
-#         verbose_name = 'Detail Author'
-#         verbose_name_plural = 'Authors'
-# 
-# class FeedsApiUrl(models.QuerySet):
-#     api_url = models.TextField(null=True)
-#     description = models.TextField(null=True)
-#     time = models.ForeignKey(TimeStampedModel,on_delete=models.CASCADE,related_name='time_stamp',db_index=True)
-#     is_remove = models.BooleanField(default=False)
-#     def published(self):
-#         return self.filter(publish=True)
-# 
-# 
-# class Feeds(TimeStampedModel):
-#     author = models.ForeignKey(Author, related_name='author_post')
-#     title = models.CharField(max_length=200)
-#     slug = models.SlugField(max_length=200, unique=True)
-#     cover = models.ImageField(upload_to='gallery/covers/%Y/%m/%d',
-#                               null=True,
-#                               blank=True,
-#                               help_text='Optional cover post')
-#     description = RedactorField()
-#     tags = models.ManyToManyField('Tag')
-#     keywords = models.CharField(max_length=200, null=True, blank=True,
-#                                 help_text='Keywords sparate by comma.')
-#     meta_description = models.TextField(null=True, blank=True)
-# 
-#     publish = models.BooleanField(default=True)
-#     objects = FeedsApiUrl.as_manager()
-#     is_remove = models.BooleanField(default=False)
-# 
-#     def get_absolute_url(self):
-#         return reverse('detail_post_page', kwargs={'slug': self.slug})
-# 
-#     @property
-#     def total_visitors(self):
-#         return Visitor.objects.filter(post__pk=self.pk).count()
-# 
-#     def __str__(self):
-#         return self.title
-# 
-#     class Meta:
-#         verbose_name = 'Detail Post'
-#         verbose_name_plural = 'Posts'
-#         ordering = ["-created"]
-# 
-# 
-# class Page(TimeStampedModel):
-#     author = models.ForeignKey(Author, related_name='author_page')
-#     title = models.CharField(max_length=200)
-#     slug = models.SlugField(max_length=200, unique=True)
-#     description = RedactorField()
-#     publish = models.BooleanField(default=True)
-#     is_remove = models.BooleanField(default=False)
-# 
-#     def __str__(self):
-#         return self.title
-# 
-#     # this will be an error in /admin
-#     # def get_absolute_url(self):
-#     #    return reverse("page_detail", kwargs={"slug": self.slug})
-# 
-#     class Meta:
-#         verbose_name = "Detail Page"
-#         verbose_name_plural = "Pages"
-#         ordering = ["-created"]
-# 
-# 
-# class Gallery(TimeStampedModel):
-#     title = models.CharField(max_length=200)
-#     attachment = models.FileField(upload_to='gallery/attachment/%Y/%m/%d')
-#     is_remove = models.BooleanField(default=False)
-# 
-#     def __str__(self):
-#         return self.title
-# 
-#     def check_if_image(self):
-#         if self.attachment.name.split('.')[-1].lower() \
-#                 in ['jpg', 'jpeg', 'gif', 'png']:
-#             return ('<img height="40" width="60" src="%s"/>' % self.attachment.url)
-#         return ('<img height="40" width="60" src="/static/assets/icons/file-icon.png"/>')
-#     check_if_image.short_description = 'Attachment'
-#     check_if_image.allow_tags = True
-# 
-#     class Meta:
-#         verbose_name = 'Detail Gallery'
-#         verbose_name_plural = 'Galleries'
-#         ordering = ['-created']
+from django.db import models
 
+
+# TODO: Need to update it according to the needs
+class Author(models.Model):
+    title = models.CharField(max_length=100)
+    is_hidden = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'self.title'
+
+
+class Stream(models.Model):
+    stream_id = models.CharField(max_length=500)
+    stream_title = models.CharField(max_length=200)
+    is_hidden = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.stream_id}'
+
+
+class Keyword(models.Model):
+    keyword = models.CharField(max_length=200)
+    is_hidden = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.keyword}'
+
+
+class Feed(models.Model):
+    title = models.CharField(max_length=200)
+    feed_id = models.CharField(max_length=200)
+    keywords = models.ManyToManyField(Keyword, null=True, blank=True)
+    stream = models.ForeignKey(Stream)
+    origin_url = models.CharField(max_length=500)
+    published_at = models.DateTimeField()
+    updated_at = models.DateTimeField(null=True, blank=True, auto_now=True)
+    summary = models.TextField()
+    engagement = models.IntegerField(default=0)
+    engagement_rate = models.FloatField(default=0.0)
+    is_hidden = models.BooleanField(default=False)
+    author = models.ForeignKey(Author)
+
+    def __str__(self):
+        return f'{self.feed_id}'
