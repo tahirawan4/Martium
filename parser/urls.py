14e0ab449461b4b112188238ad8715bc0d8e4ba1
Atizaz Abid <atizaz.abid@emblemtechnologies.com>
2017-09-19 01:36:51 +0500

    added the get feeds api
    added the api to get the feeds form the database
    initially set the limit to 10 feeds but added the optional key limit which will be send in url param this key is used to get the defined number of keys

--- parser/urls.py
+++ parser/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from parser.views import index,dash_board,get_categories
+from parser.views import index,dash_board,get_categories,get_feeds
 
 urlpatterns = [
     url(r'^$', index, name='index'),
     url(r'^dashboard$', dash_board, name='dash_board'),
     url(r'^get_categories$', get_categories, name='get_categories'),
+    url(r'^get_feeds/$', get_feeds, name='get_feeds'),
 ]
--- parser/views.py
+++ parser/views.py
@@ -4,6 +4,10 @@ import json
 import requests
 from parser.constants import FEEDLY_API_URL, FEEDLY_GET_CATEGORIES_URL, DEV_TOKEN, FEEDLY_API_URL_IDS
 from parser.utils import feed_parser
+from parser import feeds_dictionary
+from parser.models import Feed
+from django.core.paginator import Paginator
+
 
 
 def index(request):
@@ -22,3 +26,14 @@ def get_categories(request):
     feed_response_data = requests.post(FEEDLY_GET_CATEGORIES_URL, headers={'authorization': DEV_TOKEN})
     data = json.loads(feed_response_data.text)
     return JsonResponse(json.dumps(data), safe=False)
+
+
+def get_feeds(request):
+    limit = request.GET.get('limit')if 'limit' in request.GET else 10
+    feeds_list = []
+    feeds_obj = Feed.objects.filter(is_hidden=False)[0:int(limit)]
+
+    for feeds in feeds_obj:
+        feeds_list.append(feeds_dictionary(feeds))
+
+    return JsonResponse(json.dumps(feeds_list), safe=False)
