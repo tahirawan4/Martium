8121f062d4d60d0cef40b7b93a075333ccbf895a
tahirawan4 <tahirawan4@gmail.com>
2017-10-10 23:32:10 +0500

    Adding unique constraints and adding migrations. As well as run a scheduler.

--- feedly/celery.py
+++ feedly/celery.py
@@ -18,7 +18,7 @@ from parser.tasks import feed_importer
 
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
-    sender.add_periodic_task(60.0, feed_importer.s(''), name='Run After event 10 minutes')
+    sender.add_periodic_task(600.0, feed_importer.s(''), name='Run After event 10 minutes')
     # Executes every Monday morning at 7:30 a.m.
     # sender.add_periodic_task(
     #     crontab(hour=7, minute=30, day_of_week=1),
