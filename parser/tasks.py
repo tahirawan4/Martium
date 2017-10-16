65b9cdde37c7c9f8dd4eda67578b75066fba9a64
tahirawan4 <tahirawan4@gmail.com>
2017-10-16 21:54:59 +0500

    Generate urls

--- parser/tasks.py
+++ parser/tasks.py
@@ -21,6 +21,19 @@ def get_feed_content(streamId='feed/http://www.cnbc.com/id/10000115/device/rss/r
     return res.json()
 
 
+def generate_url(feed):
+    url = ''
+    if feed.get('canonicalUrl', ''):
+        url = feed.get('canonicalUrl', '')
+    else:
+        # Need to save all the alternate links
+        alternate = feed.get('alternate', [])
+        for link in alternate:
+            url = link.get('href', '')
+            break
+    return url
+
+
 @app.task
 def feed_importer(stream_id):
     # feed_url = FEEDLY_API_URL_CELERY
@@ -33,14 +46,13 @@ def feed_importer(stream_id):
             if not created:
                 feed_object.engagement = feed.get('engagement', 0)
                 feed_object.engagement_rate = feed.get('engagementRate', 0)
-                feed_object.origin_url = feed.get('canonicalUrl', '')
+                feed_object.origin_url = generate_url(feed)
                 feed_object.save()
             else:
                 author, _ = Author.objects.get_or_create(title=feed.get('author', '').lower())
                 feed_object.author = author
                 feed_object.title = feed.get('title', '')
-                feed_object.origin_url = feed.get('canonicalUrl', '')
-
+                feed_object.origin_url = generate_url(feed)
                 feed_object.summary = feed.get('summary', '').get('content', '') if feed.get('summary', '') else ''
                 feed_object.engagement = feed.get('engagement', 0)
                 feed_object.engagement_rate = feed.get('engagementRate', 0)
