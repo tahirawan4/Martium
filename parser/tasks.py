0fe0cf4a7440114eda51097b7aa693a39061e424
tahirawan4 <tahirawan4@gmail.com>
2017-10-16 21:24:10 +0500

    Correcting url

--- parser/tasks.py
+++ parser/tasks.py
@@ -33,12 +33,13 @@ def feed_importer(stream_id):
             if not created:
                 feed_object.engagement = feed.get('engagement', 0)
                 feed_object.engagement_rate = feed.get('engagementRate', 0)
+                feed_object.origin_url = feed.get('canonicalUrl', '')
                 feed_object.save()
             else:
                 author, _ = Author.objects.get_or_create(title=feed.get('author', '').lower())
                 feed_object.author = author
                 feed_object.title = feed.get('title', '')
-                feed_object.origin_url = feed.get('originId', '')
+                feed_object.origin_url = feed.get('canonicalUrl', '')
 
                 feed_object.summary = feed.get('summary', '').get('content', '') if feed.get('summary', '') else ''
                 feed_object.engagement = feed.get('engagement', 0)
